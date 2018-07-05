01186bfef606cbc2112e653aa77f54526d0e3978
Noman <noman11012@gmail.com>
2018-07-05 11:27:04 +0600

    test commit

---
+++ app/src/main/java/com/example/noman11012/gameexample/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.noman11012.gameexample;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.media.Image;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    //image button
+    private ImageButton buttonPlay;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //setting the orientation to landscape
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        //getting the button
+        buttonPlay = (ImageButton) findViewById(R.id.buttonPlay);
+
+        //adding a click listener
+        buttonPlay.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+       // starting game activity;
+        startActivity(new Intent(this, GameActivity.class));
+    }
+}
